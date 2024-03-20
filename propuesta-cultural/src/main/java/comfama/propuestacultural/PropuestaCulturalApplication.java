d445017f8a785a525712aaaa7ba479e3e83e3325
Alexis Martinez <jhonyalexis100@gmail.com>
2024-03-20 18:02:07 -0500

    finish proposedvalue

--- propuesta-cultural/src/main/java/comfama/propuestacultural/PropuestaCulturalApplication.java
+++ propuesta-cultural/src/main/java/comfama/propuestacultural/PropuestaCulturalApplication.java
@@ -2,6 +2,7 @@ package comfama.propuestacultural;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class PropuestaCulturalApplication {
@@ -9,5 +10,6 @@ public class PropuestaCulturalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PropuestaCulturalApplication.class, args);
 	}
+
 
 }
